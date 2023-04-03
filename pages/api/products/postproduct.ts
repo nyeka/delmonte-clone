874670989ba78d1ee286045ba2b0b62cc4ddda38
nyeka <nyeka2101@gmail.com>
2023-04-03 21:29:11 +0700

    feat: adding error response

--- pages/api/products/postproduct.ts
+++ pages/api/products/postproduct.ts
@@ -8,21 +8,24 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { url, image, name } = req.body;
-
-  if (req.method === "POST") {
-    if (!url || !image || !name) {
-      res.status(400).json({ message: "Data is Required" });
-      return;
+  try {
+    if (req.method === "POST") {
+      if (!url || !image || !name) {
+        res.status(400).json({ message: "Data is Required" });
+        return;
+      }
+      await Prisma.products.createMany({
+        data: {
+          image: image,
+          name: name,
+          url: url,
+        },
+      });
+      res.status(200).json({ message: "Product created", data: req.body });
+    } else {
+      res.status(400).json({ message: "Invalid request" });
     }
-    await Prisma.products.createMany({
-      data: {
-        image: image,
-        name: name,
-        url: url,
-      },
-    });
-    res.status(200).json({ message: "Product created", data: req.body });
-  } else {
-    res.status(400).json({ message: "Invalid request" });
+  } catch (error) {
+    res.status(500).json({ message: error });
   }
 }
